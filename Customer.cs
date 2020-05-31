b9343b103ce418fa289effeb50a3e2d7c49bf3ac
Jon <durrjp1@gmail.com>
2020-05-30 21:55:24 -0500

    finished part i of LINQ Practice, moving on to challenge section

---
+++ Customer.cs
@@ -0,0 +1,9 @@
+namespace LINQPractice
+{
+    public class Customer
+    {
+        public string Name { get; set; }
+        public double Balance { get; set; }
+        public string Bank { get; set; }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,12 +1,70 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace LINQPractice
 {
-    class Program
+    public class Program
     {
-        static void Main(string[] args)
-        {
-            Console.WriteLine("Hello World!");
+        public static void Main() {
+            List<Customer> customers = new List<Customer>() {
+                new Customer(){ Name="Bob Lesman", Balance=80345.66, Bank="FTB"},
+                new Customer(){ Name="Joe Landy", Balance=9284756.21, Bank="WF"},
+                new Customer(){ Name="Meg Ford", Balance=487233.01, Bank="BOA"},
+                new Customer(){ Name="Peg Vale", Balance=7001449.92, Bank="BOA"},
+                new Customer(){ Name="Mike Johnson", Balance=790872.12, Bank="WF"},
+                new Customer(){ Name="Les Paul", Balance=8374892.54, Bank="WF"},
+                new Customer(){ Name="Sid Crosby", Balance=957436.39, Bank="FTB"},
+                new Customer(){ Name="Sarah Ng", Balance=56562389.85, Bank="FTB"},
+                new Customer(){ Name="Tina Fey", Balance=1000000.00, Bank="CITI"},
+                new Customer(){ Name="Sid Brown", Balance=49582.68, Bank="CITI"}
+            };
+
+            Dictionary<string, int> millionaireList = new Dictionary<string, int>();
+            millionaireList.Clear();
+            foreach (Customer customer in customers)
+            {
+                if(customer.Bank == "WF" && customer.Balance >= 1_000_000)
+                {
+                    if(!millionaireList.ContainsKey("WF"))
+                    {
+                        millionaireList.Add("WF", 1);
+                    } else
+                    {
+                        millionaireList["WF"] += 1;
+                    }
+                } else if(customer.Bank == "BOA" && customer.Balance >= 1_000_000)
+                {
+                    if(!millionaireList.ContainsKey("BOA"))
+                    {
+                        millionaireList.Add("BOA", 1);
+                    } else
+                    {
+                        millionaireList["BOA"] += 1;
+                    }
+                } else if(customer.Bank == "FTB" && customer.Balance >= 1_000_000)
+                {
+                    if(!millionaireList.ContainsKey("FTB"))
+                    {
+                        millionaireList.Add("FTB", 1);
+                    } else
+                    {
+                        millionaireList["FTB"] += 1;
+                    }
+                } else if(customer.Bank == "CITI" && customer.Balance >= 1_000_000)
+                {
+                    if(!millionaireList.ContainsKey("CITI"))
+                    {
+                        millionaireList.Add("CITI", 1);
+                    } else
+                    {
+                        millionaireList["CITI"] += 1;
+                    }
+                }
+            };
+            foreach (KeyValuePair<string, int> item in millionaireList)
+            {
+                Console.WriteLine($"Bank: {item.Key}, Millionaires: {item.Value}");
+            }
         }
     }
 }
